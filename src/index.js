9838923bf66608d27e9f56805bd58c622fe163cb
shivamdevs <shivamsubam@gmail.com>
2022-10-30 23:05:20 +0530

    Code Update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'typeface-roboto';
+import * as serviceWorker from './serviceWorker';
+import Controller from './screens/Controller';
 
-ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
-    document.getElementById('root')
-);
+ReactDOM.render(<Controller/>, document.getElementById('root'));
+
+serviceWorker.unregister();
